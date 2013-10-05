736577a0eb8afe815c742034e1e726ae0d78bc52
yuichi.kuwahara <yuichi.kuwahara.0@gmail.com>
2013-10-05 11:31:03 +0900

    eclipse起動構成を頒布用資源に追加

--- distribution/03_copy-archetype-resource.cmd
+++ distribution/03_copy-archetype-resource.cmd
@@ -13,4 +13,14 @@ if exist "%RES_DIR%" (
 echo ## アーキタイプ資源をコピーします。
 xcopy /E /I /Q /S /EXCLUDE:%~dp0exclude-resource.txt "%WORK_DIR%\target\generated-sources\archetype\src" "%RES_DIR%"
 
+cd /D "%RES_DIR%\main\resources\archetype-resources\.settings"
+for %%i in (sit-tester-archetype*) do call :rename-file %%i
+
 pause
+goto :eof
+
+:rename-file
+set file_name=%1
+set file_name=%file_name:sit-tester-archetype=__rootArtifactId__%
+rename %1 %file_name%
+goto :eof
