1ae41149b212d75c2ad5d596aba5f0a932e609ad
yuichi.kuwahara <yuichi.kuwahara.0@gmail.com>
2015-05-31 15:08:47 +0900

    sit-tester -> sit-wt

--- distribution/03_copy-archetype-resource.cmd
+++ distribution/03_copy-archetype-resource.cmd
@@ -6,29 +6,29 @@ call "%~dp000_setbuildenv"
 set RES_DIR=%~dp0archetype\src
 
 if exist "%RES_DIR%" (
-	echo ## ƒA[ƒLƒ^ƒCƒvŽ‘Œ¹ƒfƒBƒŒƒNƒgƒŠ‚ðíœ‚µ‚Ü‚·B
+	echo ## ï¿½Aï¿½[ï¿½Lï¿½^ï¿½Cï¿½vï¿½ï¿½ï¿½ï¿½ï¿½fï¿½Bï¿½ï¿½ï¿½Nï¿½gï¿½ï¿½ï¿½ï¿½ï¿½íœï¿½ï¿½ï¿½Ü‚ï¿½ï¿½B
 	del /S /Q "%RES_DIR%" > nul
 	rmdir /S /Q "%RES_DIR%" > nul
 ) else (
 	mkdir "%RES_DIR%"
 )
 
-echo ## ƒA[ƒLƒ^ƒCƒvŽ‘Œ¹‚ðƒRƒs[‚µ‚Ü‚·B
+echo ## ï¿½Aï¿½[ï¿½Lï¿½^ï¿½Cï¿½vï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Rï¿½sï¿½[ï¿½ï¿½ï¿½Ü‚ï¿½ï¿½B
 echo ## %RES_DIR%
 xcopy /E /I /Q /S /EXCLUDE:%~dp0exclude-resource.txt "%WORK_DIR%\target\generated-sources\archetype\src" "%RES_DIR%"
 
 cd /D "%RES_DIR%\main\resources\archetype-resources\.settings"
-for %%i in (sit-tester-archetype*) do call :rename-file %%i
+for %%i in (sit-wt-archetype*) do call :rename-file %%i
 
 endlocal
 goto :eof
 
 rem 
-rem ƒtƒ@ƒCƒ‹ƒŠƒl[ƒ€••¶Žš—ñ’uŠ·
+rem ï¿½tï¿½@ï¿½Cï¿½ï¿½ï¿½ï¿½ï¿½lï¿½[ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½uï¿½ï¿½
 rem 
 :rename-file
 set in_file=%1
-set out_file=%in_file:sit-tester-archetype=__artifactId__%
+set out_file=%in_file:sit-wt-archetype=__artifactId__%
 
 echo #set( $symbol_pound = '#' )>%out_file%
 echo #set( $symbol_dollar = '$' )>>%out_file%
@@ -36,7 +36,7 @@ echo #set( $symbol_escape = '\' )>>%out_file%
 
 for /f "delims=" %%j in (%in_file%) do (
     set line=%%j
-    echo !line:${workspace_loc:/sit-tester-archetype}=${symbol_dollar}{workspace_loc:/${artifactId}}!>>%out_file%
+    echo !line:${workspace_loc:/sit-wt-archetype}=${symbol_dollar}{workspace_loc:/${artifactId}}!>>%out_file%
 )
 del %in_file%
 goto :eof
