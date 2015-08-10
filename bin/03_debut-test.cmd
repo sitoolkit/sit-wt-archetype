a8dc4137bbff775cab928554ce5559d5019658cb
yuichi.kuwahara <yuichi.kuwahara.0@gmail.com>
2015-08-10 15:25:38 +0900

    アーキタイプ作成をAnt化

--- bin/03_debut-test.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd /D %~dp0
-
-call "%~dp000_setenv"
-cd /D %~dp0..\
-call "%MVN_CMD%" -P ci -Dtester.debug=true clean test
-
-pause
--- distribution/04_install-archetype.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call "%~dp000_setbuildenv"
-
-cd /D %~dp0archetype
-
-call mvn clean install
-
-if exist "%~dp0pause.txt" pause
